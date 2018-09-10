fd6dd791d193163180b23eee5e1b93bb7a444ed5
AdamNixon <37312363+AdamNixon@users.noreply.github.com>
2018-09-10 11:14:03 +0100

    Update deformation.py

--- FemPy/deformation.py
+++ FemPy/deformation.py
@@ -38,3 +38,18 @@ def Bloodcell(x):
 	if x[2] < 0.:
 		y[2]=-y[2]	
 	return y
+
+def Pear(x):
+	y=[0, 0, 0]
+	r1 = sqrt(x[0]*x[0]+x[1]*x[1]+x[2]*x[2])
+	r0 = 4.
+	y[0] = r0*x[0]/r1
+	y[1] = r0*x[1]/r1
+
+	r3 = sqrt(y[0]*y[0]+y[1]*y[1])
+	
+	y[2]= r0*x[2]/r1
+	if(y[2]>0):	
+		if r3<2.5:
+			y[2]= sqrt(r0*r0-2.5*2.5)+1.5+1.5*cos(pi*pow(r3*r3/(2.5*2.5),0.75))	
+	return y
